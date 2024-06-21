4606d61c198a75b23ce515425782ee5e6c61ca54
namsan01 <namsan2934@gmail.com>
2024-06-21 22:47:02 +0900

    docs: 프로젝트 초기셋팅 완료

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h2 class="">안녕하세요</h2>
+  </div>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2 class="from-zinc-100">여기가 소개페이지에요</h2>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
